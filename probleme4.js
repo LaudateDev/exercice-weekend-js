3b5a06ed506bb5aaabd8816cd61cd6dd410d36d7
LaudateDev <emmanuel.tasisu@gmail.com>
2025-11-24 11:27:41 +0100

    Mise à jour Problème 4

--- probleme4.js
+++ probleme4.js
@@ -2,7 +2,7 @@ let notes = [12, 15, 10, 18, 14];
 
 let somme = 0;
 for (let i = 0; i < notes.length; i++) {
-  somme = somme + notes[i];
+  somme += notes[i];
 }
 
 let moyenne = somme / notes.length;
